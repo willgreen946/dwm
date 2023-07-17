0b9eb4abb85b198b035a3a7798fd03fc48efc1ac
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-07-17 18:35:49 +0100

    Easier on the eyes

--- config.h
+++ config.h
@@ -7,12 +7,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=8" };
 static const char gray[]        = "#262626";
-static const char white[]       = "#f0f0f0";
+static const char green[]       = "#33ff66";
 static const char black[]	= "#000000";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { white, black, black },
-	[SchemeSel]  = { white, gray,  white  },
+	[SchemeNorm] = { green, black, black },
+	[SchemeSel]  = { green, black,  green  },
 };
 
 /* scratch pads */
